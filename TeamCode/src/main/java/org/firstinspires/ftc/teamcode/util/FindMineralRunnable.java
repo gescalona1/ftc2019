8ad284ee4e1534b0c984ebbc2cefb50fa00a97e4
Giordan <gescalona@browning.edu>
2019-01-26 20:24:21 -0500

    fixv2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/FindMineralRunnable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/FindMineralRunnable.java
@@ -26,56 +26,57 @@ public final class FindMineralRunnable implements Runnable {
     public void run() {
         //Get current Hardware Map
         alreadyRunning = true;
-        while(true) {
-            if (RobotDriver.getDriver().getHardwareMap() != null) {
-                UsesHardware usesHardware = RobotDriver.getDriver().getHardwareMap();
-                OpMode opMode = (OpMode) usesHardware;
-                if (usesHardware instanceof LinearOpMode) {
-                    TFObjectDetector tfod = ((UsesHardware) opMode).getTfod();
-                    tfod.activate();
-                    if (tfod != null) {
-                        Telemetry telemetry = opMode.telemetry;
-                        // getUpdatedRecognitions() will return null if no new information is available since
-                        // the last time that call was made.
-                        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                        if (updatedRecognitions != null) {
-                            telemetry.addData("# Object Detected", updatedRecognitions.size());
-                            if (updatedRecognitions.size() == 3) {
-                                int goldMineralX = -1;
-                                int silverMineral1X = -1;
-                                int silverMineral2X = -1;
-                                for (Recognition recognition : updatedRecognitions) {
-                                    if (recognition.getLabel().equals(((UsesHardware) opMode).getLabelGoldMineral())) {
-                                        goldMineralX = (int) recognition.getLeft();
-                                    } else if (silverMineral1X == -1) {
-                                        silverMineral1X = (int) recognition.getLeft();
-                                    } else {
-                                        silverMineral2X = (int) recognition.getLeft();
-                                    }
+        while (true) {
+            UsesHardware usesHardware = RobotDriver.getDriver().getHardwareMap();
+            OpMode opMode = (OpMode) usesHardware;
+            if (usesHardware != null && usesHardware instanceof LinearOpMode) {
+                if(!((LinearOpMode) usesHardware).opModeIsActive()) return;
+                TFObjectDetector tfod = ((UsesHardware) opMode).getTfod();
+                tfod.activate();
+                if (tfod != null) {
+                    Telemetry telemetry = opMode.telemetry;
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        if (updatedRecognitions.size() == 3) {
+                            int goldMineralX = -1;
+                            int silverMineral1X = -1;
+                            int silverMineral2X = -1;
+                            for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals(((UsesHardware) opMode).getLabelGoldMineral())) {
+                                    goldMineralX = (int) recognition.getLeft();
+                                } else if (silverMineral1X == -1) {
+                                    silverMineral1X = (int) recognition.getLeft();
+                                } else {
+                                    silverMineral2X = (int) recognition.getLeft();
                                 }
-                                if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
-                                    if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
-                                        position = Position.LEFT;
-                                    } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
-                                        position = Position.RIGHT;
-                                    } else {
-                                        position = Position.CENTER;
-                                    }
-                                }else position = Position.NULL;
-                            }else position = Position.NULL;
-                            if(recordData) {
-                                telemetry.addData("Gold Mineral Position", position.toString());
-                                telemetry.update();
                             }
+                            if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                                if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                                    position = Position.LEFT;
+                                } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                                    position = Position.RIGHT;
+                                } else {
+                                    position = Position.CENTER;
+                                }
+                            } else position = Position.NULL;
+                        } else position = Position.NULL;
+                        if (recordData) {
+                            telemetry.addData("Gold Mineral Position", position.toString());
+                            telemetry.update();
                         }
                     }
-                } else {
-                    TFObjectDetector tfod = ((UsesHardware) opMode).getTfod();
-                    tfod.deactivate();
                 }
+            } else {
+                TFObjectDetector tfod = ((UsesHardware) opMode).getTfod();
+                tfod.deactivate();
+                return;
             }
         }
     }
+
 
     public static Position getCurrentPosition(){
         return position;
